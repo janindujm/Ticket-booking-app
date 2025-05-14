5a4d4aa736d93fd8e5709d36c2c1a01ad2d96b9f
janindujm <janindujm@gmail.com>
2025-05-14 16:15:15 +0530

    Add user model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -25,6 +25,20 @@ app.all('*', async( req, res, next) => {
 });
 app.use(errorHandler);
 
-app.listen(3000, () => {
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
   console.log('Auth service listening on port 3000!');
 });
+};
+
+start();
+
+
