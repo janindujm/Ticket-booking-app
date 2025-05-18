c13f025d234e098e1348b13422b4b1c31e5e69e7
janindujm <janindujm@gmail.com>
2025-05-18 15:36:26 +0530

    update signin.ts

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -35,6 +35,9 @@ const userSchema = new mongoose.Schema({
 }, {
   toJSON: {
     transform(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
       delete ret.password;
     },
   }
